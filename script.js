752780902a247ad001dc63869d34803eeaf3e2f6
sollfeliu4-ctrl <sollfeliu4@gmail.com>
2026-07-23 19:08:29 -0700

    Update script.js9

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ boton.addEventListener("click", function(){
 alert("Bienvenida al Puerto Digital 🌙⚓");
 
 });
+
+document.body.addEventListener('click', function iniciarMusica() {
+  const musica = document.getElementById('musica');
+  musica.play();
+  document.body.removeEventListener('click', iniciarMusica);
+}, { once: true });
